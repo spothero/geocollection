57e716a63980290df73ec828060bd2949eadb7d4
Brian Charous <briancharous@users.noreply.github.com>
2019-05-21 11:24:02 -0500

    Move mock collection to its own package (#3)
    Merged by your friendly neighborhood SpotBot ✅

--- test_helpers/mockcollection.go
+++ test_helpers/mockcollection.go
@@ -12,11 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package geocollection
+package testhelpers
 
 import (
-	"encoding/gob"
-
+	"github.com/spothero/geocollection"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -25,16 +24,12 @@ type MockGeoLocationCache struct {
 	mock.Mock
 }
 
-func init() {
-	gob.Register(&MockGeoLocationCache{})
-}
-
 // ItemsWithinDistance is a mocked version of ItemsWithinDistance
 func (m *MockGeoLocationCache) ItemsWithinDistance(
-	latitude, longitude, distanceMeters float64, params SearchCoveringParameters,
-) ([]int, SearchCoveringResult) {
+	latitude, longitude, distanceMeters float64, params geocollection.SearchCoveringParameters,
+) ([]int, geocollection.SearchCoveringResult) {
 	args := m.Called(latitude, longitude, distanceMeters)
-	return args.Get(0).([]int), args.Get(1).(SearchCoveringResult)
+	return args.Get(0).([]int), args.Get(1).(geocollection.SearchCoveringResult)
 }
 
 // Set is a mocked version of Set
